af6324a27a7901e7dcf5421c3c94af38c80dc6a6
Prateek Bhatnagar <bprateek10@yahoo.in>
2019-09-28 04:13:12 +0530

    updating seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "====Starting seeding data"
+user1 = User.new(first_name: "aman", last_name: "sharma", email: "aman@yopmail.com", password: "pass1234")
+
+if user1.save
+  question = user1.questions.build(content: "What is Black hole?")
+  question.save
+end
+
+user2 = User.new(first_name: "rohit", last_name: "pandey", email: "rohit@yopmail.com", password: "pass1234")
+
+if user2.save
+  question = user2.questions.build(content: "How many types of associations are present in Rails?")
+  question.save
+end
+
+puts "====Seeding data completed successfully!"
